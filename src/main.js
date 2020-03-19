9a23deebfe4c5f66532f0961a2cde71746a6eec7
Simon <3093396327@qq.com>
2020-03-19 17:00:26 +0800

    '初始化项目-axios引入\配置'

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入 element-UI 样式
 import './styles/index.less'
+import axios from 'axios' // 将 axios 共享给所有的实例使用
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 
 Vue.config.productionTip = false
 
